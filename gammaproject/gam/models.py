a94126633d24d9ec742d8055184e38bcc38a0b75
Vraj <Vrajmehta934@gmail.com>
2025-11-13 21:55:04 +0530

    models.py

--- gammaproject/gam/models.py
+++ gammaproject/gam/models.py
@@ -1,22 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
 
+class Poll(models.Model):
+question = models.CharField(max_length=255)
+created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+pub_date = models.DateTimeField(auto_now_add=True)
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-    def __str__(self):
-        return self.question_text
-
+def __str__(self):
+return self.question
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+text = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.choice_text
+def vote_count(self):
+return self.votes.count()
 
-# Create your models here.
+def __str__(self):
+return self.text
+
+class Vote(models.Model):
+poll = models.ForeignKey(Poll, related_name='votes', on_delete=models.CASCADE)
+choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+user = models.ForeignKey(User, on_delete=models.CASCADE)
+voted_at = models.DateTimeField(auto_now_add=True)
+
+class Meta:
+unique_together = ('poll', 'user')
+
+def __str__(self):
+return f"{self.user} -> {self.choice}"
